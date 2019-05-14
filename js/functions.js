ad3018d1ae19175667558a465853c8658e5020fe
Fernanda Juárez <mjuarez22@alumnos.uaq.mx>
2019-05-13 18:12:04 -0700

    animation css

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+// function aparecerMeta(){
+// 	btnCuestionario = document.getElementById("btnCuestionario");
+// 	btnCuestionario.style.display="none";
+
+// 	setTimeout(function(){
+// 		btnCuestionario.style.display="block";
+// 		btnCuestionario.style.animation="mymove 5s infinite";
+// 	}, 1000);
+// }
